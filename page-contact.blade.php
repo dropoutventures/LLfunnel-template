90a1a2eae1032aad90b6636d6ccb38663afa982d
jjjrmy <jjjrmy@users.noreply.github.com>
2021-02-24 11:17:42 -0500

    Update page-contact.blade.php

--- page-contact.blade.php
+++ page-contact.blade.php
@@ -23,7 +23,7 @@
             @if($site['branding']['address'] ?? false)
                 <div>
                     <dt class="sr-only">Postal address</dt>
-                    <dd>
+                    <dd class="flex">
                         <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
