41e37a8c8f9bf61ced0d57d748cfd55c105802a5
jjjrmy <jjjrmy@users.noreply.github.com>
2021-03-03 16:41:27 -0500

    static

--- index.blade.php
+++ index.blade.php
@@ -69,9 +69,9 @@
     <section class="container text-gray-400">
         <p class="text-xxs md:text-xs">We do not provide advice to investors and the information on this website should not be construed as such. The information which appears on our website is for information purposes only and does not constitute specific advice. Neither does it constitute a solicitation, offer or recommendation to invest in or dispose of, any investment. If you are in any doubt as to the suitability of an investment, you should seek independent financial advice from a suitable financial advisor</p>
         <ul class="flex divide-x divide-gray-300 text-gray-500 leading-none space-x-1 mt-1.5">
-            <li><a href="/pages/privacy" class="text-xs md:text-sm">Privacy Policy</a></li>
-            <li class="pl-1"><a href="/pages/terms" class="text-xs md:text-sm">Terms and Conditions</a></li>
-            <li class="pl-1"><a href="/pages/contact" class="text-xs md:text-sm">Contact Us</a></li>
+            <li><a href="/privacy" class="text-xs md:text-sm">Privacy Policy</a></li>
+            <li class="pl-1"><a href="/terms" class="text-xs md:text-sm">Terms and Conditions</a></li>
+            <li class="pl-1"><a href="/contact" class="text-xs md:text-sm">Contact Us</a></li>
         </ul>
         <hr class="my-4 border-gray-200" />
         <span class="text-sm block text-center">© 2020 {{ $site['name'] }}</span>
